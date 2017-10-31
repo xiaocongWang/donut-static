b6f441cf01538703f061a3b0a9a0709b7765c75b
王晓聪 <wangxc2@guahao.com>
2017-11-01 01:19:12 +0800

    构建配置

---
+++ build/build.js
@@ -0,0 +1,39 @@
+process.env.NODE_ENV = 'production'
+
+const ora = require('ora')    // Elegant terminal spinner (更优雅的终端片段)
+const rm = require('rimraf')  // A deep deletion module for node (like `rm -rf`)
+const path = require('path')
+const chalk = require('chalk')
+const webpack = require('webpack')
+const config = require('../config')
+const webpackConfig = require('./webpack.prod.conf')
+
+const spinner = ora('构建线上代码...')
+spinner.start()
+
+rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
+  if (err) throw err
+  webpack(webpackConfig, (err, stats) => {
+    spinner.stop()
+    if (err) throw err
+
+    process.stdout.write(stats.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false
+    }) + '\n\n')
+
+    if (stats.hasErrors()) {
+      console.log(chalk.red(' Build faild with errors.\n'))
+      process.exit(1)
+    }
+
+    console.log(chalk.cyan(' Build complete.\n'))
+    console.log(chalk.yellow(`
+      Tip: built files are meant to be served over an HTTP server.
+      Opening index.html over file:// won\'t work.
+    `))
+  })
+})
---
+++ build/utils.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const config = require('../config')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+exports.assetsPath = function(_path) {
+  const assetsSubDirectory = process.env.NODE_ENV === 'production'
+    ? config.build.assetsSubDirectory
+    : config.dev.assetsSubDirectory
+  return path.posix.join(assetsSubDirectory, _path)
+}
+
+exports.cssLoaders = function(options={}) {
+  const cssLoader = {
+    loader: 'css-loader',
+    options: {
+      minimize: process.env.NODE_ENV === 'production',
+      sourceMap: options.sourceMap
+    }
+  }
+
+  function generateLoaders(loader, loaderOptions) {
+    const loaders = [cssLoader];
+    loaders.push({
+      loader: loader,
+      options: Object.assign({}, loaderOptions, {
+        sourceMap: options.sourceMap
+      })
+    })
+
+    if (options.extract) {
+      return ExtractTextPlugin.extract({
+        ues: loaders,
+        fallback: 'vue-style-loader'
+      })
+    } else {
+      return 'vue-style-loader'.concat(loaders)
+    }
+  }
+
+  return {
+    css: generateLoaders(),
+    postcss: generateLoaders(),
+    less: generateLoaders('less'),
+    sass: generateLoaders('sass', { indentedSyntax: true }),
+    scss: generateLoaders('sass'),
+    stylus: generateLoaders('stylus'),
+    styl: generateLoaders('stylus')
+  }
+}
+
+// Generate loaders for standalone style files (outside of .vue)
+exports.styleLoaders = function(options) {
+  const output = [];
+  const loaders = exports.cssLoaders(options);
+  for (const extension in loaders) {
+    const loader = loaders[extension]
+    output.push({
+      test: new RegExp('\\.' + extension + '$'),
+      use: loader
+    })
+  }
+  return output
+}
