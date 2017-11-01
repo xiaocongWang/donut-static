b1f9e66374da1940c5560278550c5853e0a422f6
王晓聪 <wangxc2@guahao.com>
2017-11-01 17:50:45 +0800

    mock数据

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,15 @@
       <div class="img-wrap">
         <img src="static/img/icon.jpg" alt="">
       </div>
-      <p>{{msg}}</p>      
+      <p>{{msg}}</p>
+    </section>
+    <section>
+      <p>这是一个列表</p>
+      <ul>
+        <li v-for="item of newList" :key="item.id">
+          {{item.content}}
+        </li>
+      </ul>
     </section>
   </div>
 </template>
@@ -13,15 +21,19 @@
 import { getList } from '../api';
 
 export default {
-  name: 'HelloWorld',
+  name: 'home',
   data() {
     return {
       msg: 'Welcome to Donut!',
+      newList: [],
     };
   },
   methods: {
     getList() {
-      getList();
+      getList().then((res) => {
+        const { data } = res;
+        this.newList = data;
+      });
     },
   },
   mounted() {
@@ -31,6 +43,12 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  section
+    margin-bottom 50px
+  p
+    margin 10px
+  li
+    margin-bottom 10px
   .img-wrap
     img
       width 500px
